eea2abe627e7401f2eff95e95460289c748b8028
Matthew Layne <matthew@punchlinerecords.com>
2025-03-17 00:50:45 -0700

    Fixed frontend and deployed locally

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "https://spnsr-api.onrender.com"; // Replace with 
-your actual backend URL;
+const API_BASE_URL = "https://spnsr-api.onrender.com";
 
 export default API_BASE_URL;
 
